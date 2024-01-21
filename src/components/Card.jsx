da34a5e3a1a41d9872d9b7857a28d5710d157131
JesusVA760 <tonio4340406@gmail.com>
2024-01-20 21:22:29 -0800

    progress

---
+++ src/components/Card.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+import Star from "../images/icon-star.svg";
+import Plus from "../images/icon-plus.svg";
+import "./Card.css";
+
+const Card = () => {
+  return (
+    <div className="card-body">
+      <div className="container">
+        <div className="card">
+          <div className="card-header">
+            <h1>
+              <img src={Star} />
+              FAQs
+            </h1>
+          </div>
+          <div className="card-content">
+            <div className="card-question">
+              <h3>
+                What is Frontend Mentor, and how will it help me?{" "}
+                <img src={Plus} />
+              </h3>
+            </div>
+            <div className="card-question">
+              <h3>
+                Is Frontend Mentor free? <img src={Plus} />
+              </h3>
+            </div>
+            <div className="card-question">
+              <h3>
+                Can I use frontend Mentor projects in my portfolio?{" "}
+                <img src={Plus} />
+              </h3>
+            </div>
+            <div className="card-question">
+              <h3>
+                How can I get help if I'm stuck on a challenge?{" "}
+                <img src={Plus} />
+              </h3>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
